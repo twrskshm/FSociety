7b91172fc8c3fd3031de0a2617b20c2617856d5e
Saksham Tiwari <contact@tsaksham.com>
2021-02-06 02:05:42 -0500

    Google registration done.
    Google registration done.

---
+++ app/src/main/java/com/project/fsociety/PageOne.java
@@ -0,0 +1,14 @@
+package com.project.fsociety;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PageOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_one);
+    }
+}
--- app/src/main/java/com/project/fsociety/Register.java
+++ app/src/main/java/com/project/fsociety/Register.java
@@ -4,40 +4,76 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class Register extends AppCompatActivity {
-    private GoogleSignInClient mGoogleSignInClient;
-    private final int RC_SIGN_IN = 1;
+    private final static int RC_SIGN_IN = 123;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        Button googleRegistration = findViewById(R.id.google_registration);
-        googleRegistration.setOnClickListener(v -> {
-            createRequest();
-            signIn();
+        mAuth = FirebaseAuth.getInstance();
+
+        Button googleRegistrationButton = findViewById(R.id.google_registration);
+        googleRegistrationButton.setOnClickListener(v -> {
+            googleRegistration();
         });
     }
 
-    void createRequest() {
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+    private void googleRegistration() {
+        GoogleSignInOptions GSO = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id ))
                 .requestEmail()
                 .build();
 
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-    }
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, GSO);
 
-    private void signIn() {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                Toast.makeText(this, "Authentication failed (1).", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = mAuth.getCurrentUser();
+
+                        Intent intent = new Intent(this, PageOne.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(this, "Authentication failed (2).", Toast.LENGTH_LONG).show();
+                    }
+                });
     }
 }
